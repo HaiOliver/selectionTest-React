8a492cdbd4be49a5f307006f0af347d41a47ef0f
HaiOliver <hhuynh28@my.bcit.ca>
2021-04-19 15:55:32 -0700

    creating backend server Nodejs

--- src/components/park/park.jsx
+++ src/components/park/park.jsx
@@ -7,25 +7,24 @@ export default function Park() {
       return (
             <Container>
                   <Row>
-                        <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="" />
-                        <Card.Body>
-                        <Card.Title>Location: test</Card.Title>
-                        <Card.Text>
-                              <h3>Time: 10PM</h3>
-                              <h3>Number of ducks are fed: 100 </h3>
-                              <h3>What kind of food: bread</h3>
-                              <h3>How many food: 1kg</h3>
-                              
-                        </Card.Text>
-                        </Card.Body>
-                        {/* <ListGroup className="list-group-flush">
-                        <ListGroupItem>Cras justo odio</ListGroupItem>
-                        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                        </ListGroup> */}
+                        <Card border="dark" style={{ width: '21rem' }}>
+                              <Card.Header>
+                                    <h2>
+                                          Location: test
+                                    </h2>
+                              </Card.Header>
+                              <Card.Body>
+                                    <Card.Title>
+                                          <h4>Time: 10PM</h4>
+                                          <h4>Number of ducks are fed: 100 </h4>
+                                          <h4>What kind of food: bread</h4>
+                                          <h4>How many food: 1kg</h4>
+                                    </Card.Title>
 
+                              </Card.Body>
                         </Card>
+
+
                   </Row>
             </Container>
       )
--- src/pages/list-all-parks-fed-duck/list-parks.jsx
+++ src/pages/list-all-parks-fed-duck/list-parks.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row';
 import Park from '../../components/park/park';
+import Col from 'react-bootstrap/Col'
 export default function listParks() {
       return (
-            <Container>
+            <Container >
                   <Row>
-                        <Park/>
+                        <Col>
+                              <h1>
+                               All parks that fed ducks list below
+                              </h1>
+                        </Col>
+                  </Row>
+                  <Row>
+                        <Col  xs={12} md={3}>
+                              <Park/>
+                        </Col>
+                        <Col xs={12} md={3}>
+                              <Park/>
+                        </Col>
+                        <Col xs={12} md={3}>
+                              <Park/>
+                        </Col>
+
+                        <Col xs={12} md={3}>
+                              <Park/>
+                        </Col>
+
                   </Row>
             </Container>
       )
